2ba9771bab6bf62b6140562dc8b98d20969206ff
ganeshp <ganesh_pachpind@external.mckinsey.com>
2017-06-04 13:54:20 +0530

    Added page query parameter for pagination

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require('express')
-const path = require('path');
+const path = require('path')
+const fs = require('fs')
 const app = express()
 
 app.get('/reviews.json', function (req, res) {
-  // res.send('Hello World!')
-   res.sendFile(path.normalize(__dirname + '/reviews.json'))
+  var jsonObj = JSON.parse(fs.readFileSync(__dirname + '/reviews.json', 'utf8'));
+  var responseObj= {
+    status: jsonObj.status,
+    total_reviews: jsonObj.total_reviews,
+    data:[]
+  }
+  console.log();
+    if(req.query){
+      var page = req.query['page'];
+      if(page) {
+        var start = page * 10;
+        for( var i = start  ; i < (start+10) && jsonObj.data.length; i++) {
+           responseObj.data.push(jsonObj.data[i]);
+        }
+      }
+    }
+  res.send(responseObj);
 })
 
 app.listen(process.env.PORT || 8000, function () {
-  console.log('Example app listening on port 8000!')
+  console.log('stub app started')
 })
