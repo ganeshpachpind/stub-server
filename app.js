799feef8fd4961326207fff6616dd9dfed3814f0
ganeshp <ganesh_pachpind@external.mckinsey.com>
2017-06-04 11:34:17 +0530

    Simple stub server setup

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path');
+const app = express()
+
+app.get('/reviews.json', function (req, res) {
+  // res.send('Hello World!')
+   res.sendFile(path.normalize(__dirname + '/reviews.json'))
+})
+
+app.listen(8000, function () {
+  console.log('Example app listening on port 8000!')
+})
